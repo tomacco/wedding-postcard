9b4d56e6bff59ff2fa09c8bac5316af4caabb8dc
Oscar Guillén <osguima3@gmail.com>
2020-09-05 20:59:17 +0200

    Initial version

---
+++ recipients.js
@@ -0,0 +1,18 @@
+let recipientsParam = new URL(window.location.href).searchParams.get("recipients");
+let recipients = recipientsParam === null ? [ "Lorem Ipsum", "Dolor Sit Amet" ] : recipientsParam.split(",");
+
+let html = "";
+let i = 0;
+for (; i < recipients.length; i++) {
+    html += "<p>" + recipients[i] + "</p><div class=\"line\">";
+}
+
+for (; i < 5; i++) {
+    html += "<p style='color: white'>-</p><div class=\"line\">";
+}
+
+let recipientsDiv = document.getElementById("recipients_list");
+recipientsDiv.innerHTML = html;
+fitText(recipientsDiv, 1.7);
+fitText(document.getElementById("recipients_header"), 1.5);
+fitText(document.getElementById("info"), 2.2);
