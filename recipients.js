fadb03a1d75811b2921ddc14a377380018116c57
Ivan Gonzalez <ivan.gonzalez@me.com>
2020-09-05 23:57:59 +0200

    Added missing closing </div> Added flexbox to the card so it can be centered without tables Added onclick flip effect

--- recipients.js
+++ recipients.js
@@ -1,18 +1,30 @@
-let recipientsParam = new URL(window.location.href).searchParams.get("recipients");
-let recipients = recipientsParam === null ? [ "Lorem Ipsum", "Dolor Sit Amet" ] : recipientsParam.split(",");
 
-let html = "";
-let i = 0;
-for (; i < recipients.length; i++) {
-    html += "<p>" + recipients[i] + "</p><div class=\"line\">";
+
+document.addEventListener('DOMContentLoaded', (event) => {
+	let recipientsParam = new URL(window.location.href).searchParams.get("recipients");
+	let recipients = recipientsParam === null ? [ "Lorem Ipsum", "Dolor Sit Amet" ] : recipientsParam.split(",");
+	let html = "";
+	let i = 0;
+	for (; i < recipients.length; i++) {
+	    html += "<p>" + recipients[i] + "</p><div class=\"line\"></div>";
+	}
+
+	for (; i < 5; i++) {
+	    html += "<p style='color: white'>-</p><div class=\"line\"></div>";
+	}
+	console.log(html);
+	let recipientsDiv = document.getElementById("recipients_list");
+	recipientsDiv.innerHTML = html;
+	fitText(recipientsDiv, 1.7);
+	fitText(document.getElementById("recipients_header"), 1.5);
+	fitText(document.getElementById("info"), 2.2);
+	//.flip-card:hover .flip-card-inner ->.flipped
+});
+
+let flipCard = function() {
+	let innerCard = document.getElementById('innerCard');
+	let classList = innerCard.classList;
+	innerCard.classList.toggle('flipped');
 }
 
-for (; i < 5; i++) {
-    html += "<p style='color: white'>-</p><div class=\"line\">";
-}
 
-let recipientsDiv = document.getElementById("recipients_list");
-recipientsDiv.innerHTML = html;
-fitText(recipientsDiv, 1.7);
-fitText(document.getElementById("recipients_header"), 1.5);
-fitText(document.getElementById("info"), 2.2);
